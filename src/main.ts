f67c457bbad35b5094a45b51654088425e2ea18d
wangyifeng <2665939276@qq.com>
2022-12-08 15:52:52 +0800

    fix: 国际化打包问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.min.js'
+import locale from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import "@/assets/css/common.less"
 
@@ -13,7 +13,7 @@ import App from "./App.vue";
 
 const app = createApp(App);
 app.use(ElementPlus, {
-  locale: zhCn,
+  locale: locale,
 })
 app.use(router)
 app.use(createPinia())
